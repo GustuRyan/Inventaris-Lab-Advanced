a3bcf48353f8775622fbd3e211ea1363be0e1a00
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-01-04 23:31:22 +0800

    update the color in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,18 @@ export default {
     "./resources/**/*.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-gradient': 'linear-gradient(to right, #EE2E45 75%, transparent 25%)',
+      },
+      colors: {
+        'light-red' : '#FFEDEF',
+        'bold-red' : '#EE2E45',
+        'light-blue' : '#F0F6FF',
+        'bold-blue' : '#292F3E',
+        'primary-green' : '#1DC88A',
+      }
+    },
   },
   plugins: [],
   
