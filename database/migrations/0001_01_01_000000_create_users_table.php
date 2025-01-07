ea4678139014b5ecedd9fc230e92f4f6e513ca1b
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-01-07 23:37:23 +0800

    update users table migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,6 +19,10 @@ return new class extends Migration
             $table->string('phone')->unique();
             $table->string('password');
             $table->timestamp('email_verified_at')->nullable();
+
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+            
             $table->rememberToken();
             $table->timestamps();
         });
