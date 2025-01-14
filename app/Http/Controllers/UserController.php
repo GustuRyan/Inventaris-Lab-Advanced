2c4a89bdf73584fb87dcb68408c986e33b3f0bc7
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-01-14 23:51:59 +0800

    update UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,8 +4,47 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Models\Role;
+use App\Models\Employee;
 
 class UserController extends Controller
 {
-    
+    public function index()
+    {
+        $roles = Role::all();
+
+        $employeeUserIds = Employee::pluck('user_id');
+
+        $guests = User::where('role_id', 1)
+            ->whereNotIn('id', $employeeUserIds)
+            ->get();
+
+        $students = User::where('role_id', 2)
+            ->whereNotIn('id', $employeeUserIds)
+            ->get();
+            
+        $employees = User::whereIn('id', $employeeUserIds)->get();
+
+        return view('admin.pages.role.index', compact('guests', 'students', 'employees', 'roles'));
+    }
+
+
+    public function updateRole(Request $request, $id)
+    {
+        // Validasi input
+        $request->validate([
+            'role_id' => 'required|integer',
+        ]);
+
+        // Cari transaksi berdasarkan ID
+        $user = User::findOrFail($id);
+
+        // Update status
+        $user->role_id = $request->role_id;
+        $user->save();
+
+        // Redirect kembali dengan pesan sukses
+        return redirect()->back()->with('success', 'Role pengguna berhasil diperbarui!');
+    }
 }
