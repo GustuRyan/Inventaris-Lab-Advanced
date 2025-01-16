3f30399e43898b970187473cf9686f480c567004
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-01-16 22:06:10 +0800

    update materials migration

--- database/migrations/2024_06_11_151910_create_materials_table.php
+++ database/migrations/2024_06_11_151910_create_materials_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->text('material_name');
             $table->text('character');
             $table->text('condition');
-            $table->integer('stocks');
         });
     }
 
