fa96271997e6f24cd9ed3695a99188252cb7f219
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-01-18 21:20:24 +0800

    update main database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,21 +15,22 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
-        // User::factory()->create([
-        //     'username' => 'admin',
-        //     'name' => 'Administrator',
-        //     'email' => 'admin@example.com',
-        //     'phone' => '1234567890',
-        //     'password' => Hash::make('123456'),
-        // ]);
-
-        // $this->call(RoomSeeder::class);
-        // $this->call(MaterialSeeder::class);
-        // $this->call(ToolSeeder::class);
-        // $this->call(RoomDetailSeeder::class);
-        // $this->call(RecommendSeeder::class);
         $this->call(RoleSeeder::class);
-        $this->call(UserRoleSeeder::class);
+
+        User::factory()->create([
+            'username' => 'admin',
+            'name' => 'Administrator',
+            'email' => 'admin@example.com',
+            'phone' => '1234567890',
+            'password' => Hash::make('123456'),
+            'role_id' => 5,
+        ]);
+
+        $this->call(RoomSeeder::class);
+        $this->call(EmployeeSeeder::class);
+        $this->call(MaterialSeeder::class);
+        $this->call(ToolSeeder::class);
+        $this->call(RoomDetailSeeder::class);
+        $this->call(RecommendSeeder::class);
     }
 }
