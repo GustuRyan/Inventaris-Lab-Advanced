1bb6247cb68fbf9b3abbe3dd9c055d783b3bed6f
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-03-15 22:27:04 +0800

    update Employee Seeder

--- database/seeders/EmployeeSeeder.php
+++ database/seeders/EmployeeSeeder.php
@@ -7,9 +7,6 @@ use Illuminate\Support\Facades\DB;
 
 class EmployeeSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
         DB::table('employees')->insert([
