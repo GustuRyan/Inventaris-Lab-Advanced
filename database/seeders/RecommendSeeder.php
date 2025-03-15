340d139e2e81370252e020f43120f7a5fa8079b6
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-03-15 22:27:23 +0800

    update Recommend Seeder

--- database/seeders/RecommendSeeder.php
+++ database/seeders/RecommendSeeder.php
@@ -8,9 +8,6 @@ use Illuminate\Support\Facades\DB;
 
 class RecommendSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
         $recommends = [
